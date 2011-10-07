89dacdb9898b8969ba7f7be5fc217590fd734d71
kyoDralliam <chocobodralliam@gmail.com>
2011-10-07 18:41:23 +0200

    wip (work in progress)

--- lexer.mli
+++ lexer.mli
@@ -1,2 +1,28 @@
+(**
 
+éléments du lexer :
+
+identifiantMajuscule     UID
+identifiantminuscule     LID
+<                        LESS
+>                        GREATER
+(                        LPAREN
+)                        RPAREN
+->                       ARROW
+;                        SEMI
+entier                   INT
+,                        COMMA
+[                        LSQBR
+]                        RSQBR
+{                        LBRACK
+}                        RBRACK
+.                        DOT
+..                       DOTDOT
++                        PLUS
+-                        MINUS
+*                        TIMES
+/                        DIV
+%                        MOD
+
+*)
 
