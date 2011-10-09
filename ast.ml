b835c4a3338e3022fe010e6f2da5940d57cdab9c
kyoDralliam <chocobodralliam@gmail.com>
2011-10-09 11:40:14 +0200

    ast updated
    fusion de la version de guillaume et de ma propre version

--- ast.ml
+++ ast.ml
@@ -1,62 +1,105 @@
-
-
-(* Pour ne pas faire de confusions dans les types*)
 type id = string
 
-(* opérateurs employés dans les expressions arithmétiques *)
-type operator = Plus | Minus | Times | Div | Mod
-(* expressions arithmétiques : les cas de base peuvent être des variables ou des litéraux *)
-type intExpr = Var of id | Int of int | Operation of Operator * IntExpr * IntExpr
+(* Les opérateurs utilisés dans les expressions arithmétiques. *)
+type binary_op =
+  | Plus | Minus | Times | Div | Mod
 
-(* un intervalle : employé lorsque l'on sélectionne une partie d'un fil épais *)
-type range = { min : intExpr ; max : intExpr }
+type unary_op =
+  | Neg
 
+(* Une expression arithmétique construite à partir de nombres et de variables *)
+type integer =
+  | Int of int
+  | Var of id
+  | Binary_Op of binary_op * integer * integer
+  | Unary_Op of unary_op * integer
 
-
-(* un fil en tant que rvalue *)
-type wire_rvalue = id * int
-
-(* 
-Un fil en tant que lvalue. 
-On distingue :
-   - un fil qu a été passsé en input
-   - un fil sortant d'un block déclaré localement
-   - une partie d'un fil épais
-   - la fusion de plusieurs fils pour obtenir un fil épais
+(* Un fil nommé est repéré par le nom du bloc auquel il appartient et par son
+   propre nom. 
+   Attention conflit ! On a le choix entre :
+   - garder block de type id (donc string) et de considérer que
+     pour les entrées et les sorties du bloc courant, block =  ""
+   - changer block en type id option ce qui sémantiquement me semble 
+     plus correct, et mettre block = None pour les entrées et sorties
+     du bloc courant.
 *)
-type wire_lvalue = 
-    SimpleWire of id 
-  | BlockWire of id * id  
-  | SliceWire of wire_lvalue * range 
-  | MergeWire of wire_lvalue list
+type wire_identifier =
+    {
+      block : id (* ou id option *)
+      wire : id
+    }
 
-(* 
-Une instruction décrivant le corps d'un block.
-Elle est composée :
-   - d'un identifiant indiquant le block instancié
-   - d'un identifiant auquel est lié le block créé
-   - d'une liste de fils passés au block lors de la création
-*)
-type instruction = id * id * wire_lvalue list
+(* Une partie (contiguë) d'un fil *)
+type slice =
+    {
+      wire : wire
+      min : integer
+      max : integer
+    }
 
-(* une sortie est un couple composé d'une étiquette et d'un fil *)
-type output = id * wire_lvalue
+(* Un fil :
+    Named_Wire : Un fil nommé (entrée ou sortie du bloc courant, ou
+                               sortie d'un sous bloc)
+    Merge : Réunion de plusieurs fils en un seul gros fil
+    Slice : Une partie (contiguë) d'un fil *)
+and type wire =
+  | Named_Wire of wire_identifier
+  | Merge of wire list
+  | Slice of slice
 
-(*
-Un block. Se décompose en :
-  - un identifiant
-  - des paramètres entiers sur lesquels faire une éventuelle récursion
-  - des fils en entrée
-  - des instructions composant son corps
-  - des fils en sortie
-*)
-type block = {
-  id : id ;
-  params : intExpr list;
-  inputs : wire_rvalue list;
-  instructions : instruction list;
-  outputs : output list
-}
+(* Un paramètre peut être une variable littérale (cas général de la récursion)
+   ou un nombre (cas limite de la récursion). *)
+type parameter =
+  | Parameter_Name of id
+  | Parameter_Value of int
+
+(* Définition d'un fil : on le déclare, et on indique
+   à quel fil il est connecté. *)
+type wire_definition =
+    {
+      declaration : wire_declaration
+      value : wire
+    }
+
+(* Le type d'un bloc (par exemple Adder<42> pour un additionneur 42 bits). *)
+type block_type =
+    {
+      name : id
+      parameter : integer list
+    }
+
+(* Instanciation d'un type de bloc, c'est à dire définition d'un bloc.
+   On donne le type du bloc, son nom et ses entrées. *)
+type instantiation =
+    {
+      block_type : block_type
+      name : id
+      input : wire list
+    }
+
+(* Déclaration d'un fil : on définit son nom et sa taille. *)
+type wire_declaration =
+    {
+      name : id
+      size : integer
+    }
+
+(* Définition d'un type de bloc *)
+type block_type_definition =
+    {
+      name : id
+      parameter : parameter list
+      input : wire_declaration list
+      instantiations : instantiation list
+      output : wire_definition list
+    }
+
+(* Définition d'un type de périphérique *)
+type device_type_definition =
+    {
+      name : id
+      refresh_period : int
+    }
 
 
 (* 
@@ -64,4 +107,8 @@ Un circuit combinatoire :
    - un identifiant correspondant au bloc global
    - une liste des blocs définis dans le code
 *)
-type circuit = id * block list
+type circuit = 
+    {
+      name : id ;
+      block_definitions : block list
+    }
