c41e391c568f2d643e08850953bbc581a143b3aa
Kenji Maillard <chocobodralliam@gmail.com>
2011-12-01 21:27:25 +0100

    option -label & -susucre rajoutée

--- FrontEnd/main.ml
+++ FrontEnd/main.ml
@@ -22,6 +22,7 @@ let setup_arg_parsing () =
   let output_o = ref "" in
   let graph_style = ref Graph2 in 
   let cc = ref "" in
+  let label = ref false in
 
   let sources = ref [] in
      
@@ -31,13 +32,15 @@ let setup_arg_parsing () =
     "-analyse", Set_string output_analyse, "file   output the result of the semantic analysis in file";
     "-graph", Set_string output_graph, "file   output the graph in file";
     "-pdf", Set_string output_graph_pdf, "file   output the pdf resulting from the graph in file";
+    "-label", Set label, "switch on the label on the pdf's arrows";
     "-simulator", Set_string output_simulator, "file   output the result of compiling as specified in simulator.ml in file";
     "-simulatorV2", Set_string output_simulatorV2, "file   output the result of compiling as specified in simulatorV2.ml in file";
     "-c", Set_string output_c, "file   output the result of compiling in c in file";
     "-o", Set_string output_o, "file   output the result of compiling in file" ;
     "-graph1", Unit (fun () -> graph_style := Graph1), "set the graph setting to graph1 (Damien)" ;
     "-graph2", Unit (fun () -> graph_style := Graph2), "set the graph setting to graph2 (Kenji)" ;
-    "-cc", Set_string cc, "val   set the c compiler to val (default $(cc) or, if not found, gcc)"
+    "-cc", Set_string cc, "val   set the c compiler to val (default $(cc) or, if not found, gcc)" ;
+    "-susucre", Set_int (ref 0), "n   set the number of sugar put in your tea or coffee"
   ] in 
     
   let annon_fun s = sources := s::!sources in
@@ -55,7 +58,8 @@ let setup_arg_parsing () =
      !output_o,
      !sources,
      !graph_style,
-     !cc)
+     !cc,
+     !label)
     
 
 let lex_source source_content output_lex =
@@ -111,7 +115,7 @@ let analyse_ast ast output_analyse =
   with e -> analyse_exception e ; exit 3
 
 (* FIXME : implémenter Graph1 *)
-let create_graph graph_style analysed_ast output_graph output_graph_pdf =
+let create_graph graph_style analysed_ast output_graph output_graph_pdf label =
   match graph_style with
     | Graph1 -> Graph1Type ([||],[],[],[]) 
     | Graph2 -> 
@@ -122,7 +126,9 @@ let create_graph graph_style analysed_ast output_graph output_graph_pdf =
 	  end;
 	  begin 
 	    if output_graph_pdf <> ""
-	    then ignore (mk_pdf output_graph_pdf (Print.GraphPrinter.graph g))
+	    then 
+	      let graph_string = Print.GraphPrinter.graph ~label g in
+		ignore (mk_pdf output_graph_pdf graph_string)
 	  end;
 	  Graph2Type graph
 
@@ -188,13 +194,14 @@ let _ =
        output_o,
        sources,
        graph_style,
-       cc) = setup_arg_parsing () in
+       cc,
+       label) = setup_arg_parsing () in
 
     let source_content = get_files_content sources in
     let lexbuf = lex_source source_content output_lex in
     let ast = parse_lexbuf lexbuf output_parse in
     let analysed_ast = analyse_ast ast output_analyse in
-    let graph = create_graph graph_style analysed_ast output_graph output_graph_pdf in
+    let graph = create_graph graph_style analysed_ast output_graph output_graph_pdf label in
     let _ = create_simulator graph output_simulator in
     let _ = create_simulatorV2 graph output_simulatorV2 in
     let cpp_source_file = create_cpp_source graph output_cpp in
